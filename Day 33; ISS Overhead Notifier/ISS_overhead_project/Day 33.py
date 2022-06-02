925a8b4a61d91b50022ef46b4ebcc439388345d1
Raymond Visser <84017521+Halaszraymond@users.noreply.github.com>
2022-06-02 15:30:42 +0200

    Add files via upload

---
+++ Day 33; ISS Overhead Notifier/ISS_overhead_project/Day 33.py
@@ -0,0 +1,52 @@
+import requests
+from datetime import datetime
+import smtplib
+
+MY_LAT = 52.300739 # Your latitude
+MY_LONG = 5.627950 # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+position_close_to_iss = False
+# Your position is within +5 or -5 degrees of the ISS position.
+if MY_LAT-5 <+ iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+    position_close_to_iss = True
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0]) + 2
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0]) + 2
+
+time_now = datetime.now()
+current_hour = time_now.hour
+
+# If the ISS is close to my current position
+# and it is currently dark
+if position_close_to_iss and sunrise > current_hour > sunset:
+# Then send me an email to tell me to look up.
+    my_email = f"geenideejantje@yahoo.com"
+    password = f"Zt$%5b4R&62kY!"
+    with smtplib.SMTP("smtp.mail.yahoo.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="raymond@halasz.nl",
+                            msg=f"Subject:Look up!\n\nThe ISS is flying over right now."
+                            )
+
+# BONUS: run the code every 60 seconds.
+
+
+
