6075c1755a5a100d1c955c120c79d60de94c99a8
Raymond Visser <84017521+Halaszraymond@users.noreply.github.com>
2022-06-08 15:27:03 +0200

    Update notification_manager.py

--- Day 39; Flight Deal Finder Capstone Project/notification_manager.py
+++ Day 39; Flight Deal Finder Capstone Project/notification_manager.py
@@ -7,10 +7,10 @@ class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
 
     def __init__(self):
-        self.TWILIO_SID = "AC09a26062f8573863cc0fb7ec5800f409"
-        self.TWILIO_AUTH_TOKEN = "4561ac6edf3f26a2ca4acfd696ac844f"
-        self.TWILIO_VIRTUAL_NUMBER = "+19784806248"
-        self.TWILIO_VERIFIED_NUMBER = "+31613344119"
+        self.TWILIO_SID = "{TWILIO_SID}"
+        self.TWILIO_AUTH_TOKEN = "{TWILIO_AUTH_TOKEN}"
+        self.TWILIO_VIRTUAL_NUMBER = "{TWILIO_PHONE_NUMBER"
+        self.TWILIO_VERIFIED_NUMBER = "{YOUR_PHONE_NUMBER}"
         self.client = Client(self.TWILIO_SID, self.TWILIO_AUTH_TOKEN)
 
     def send_message(self, message):
@@ -23,9 +23,9 @@ class NotificationManager:
 
     def send_email(self, message):
         emails = Day_40().list_of_emails()
-        my_email = "geenideejantje@yahoo.com"
-        password = "okxjeqagdthrkfpt"
-        with smtplib.SMTP("smtp.mail.yahoo.com", 587) as connection:
+        my_email = "{YOUR_EMAIL_ADDRESS}"
+        password = "{YOUR_PASSWORD}"
+        with smtplib.SMTP("smtp.?.com", 587) as connection:
             connection.starttls()
             connection.login(user=my_email, password=password)
             for receiver_email in emails:
