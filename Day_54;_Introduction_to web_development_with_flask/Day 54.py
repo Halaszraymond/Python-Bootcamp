daad6b2818490b2dae6fe3a943b85d5e1bfda34a
Raymond Visser <84017521+Halaszraymond@users.noreply.github.com>
2022-06-23 20:14:31 +0200

    Add files via upload

---
+++ Day_54;_Introduction_to web_development_with_flask/Day 54.py
@@ -0,0 +1,31 @@
+import time
+
+current_time = time.time()
+print(current_time)
+
+
+def speed_calc_decorator(function):
+    def wrapper_funtion():
+        start_time = time.time()
+        function()
+        end_time = time.time()
+        function()
+        print(f"{function.__name__} run speed: {end_time - start_time}s")
+
+    return wrapper_funtion
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        var = i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(100000000):
+        var = i * i
+
+
+fast_function()
+slow_function()
